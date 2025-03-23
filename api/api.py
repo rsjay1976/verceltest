7ee1bf23de702ed3f07474bfd27ab39151944d58
unknown <jayaram.s.subramanian@oracle.com>
2025-03-23 22:53:50 +0530

    added requets lbrary

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 # api.py
 from fastapi import FastAPI
-import subprocess
+#import subprocess
+import requests
 app = FastAPI()
 
 def run_httpie():
@@ -11,18 +12,21 @@ def run_httpie():
 #     capture_output=True,
 #     text=True
 # )
-        result = subprocess.run(
-    ["http", "get", "https://httpbin.org/get?email=22f3002723@ds.study.iitm.ac.in"],
-    capture_output=True,
-    text=True
-)
+#         result = subprocess.run(
+#     ["http", "get", "https://httpbin.org/get?email=22f3002723@ds.study.iitm.ac.in"],
+#     capture_output=True,
+#     text=True
+# )
+            url = "https://httpbin.org/get"
+            params = {"email": "22f3002723@ds.study.iitm.ac.in"}
+            response = requests.get(url, params=params)
         # result = subprocess.run(
         # ["httpie", "get", "https://httpbin.org/get?email=22f3002723@ds.study.iitm.ac.in"],
         # capture_output=True,
         # text=True
         # )
  
-        return  result.stdout
+            return  response.json()
     except Exception as e:
         return {"error": str(e)}
 
